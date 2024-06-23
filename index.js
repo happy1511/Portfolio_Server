0cf993cccabd505a20e454d68cddd958a9fb70cb
Happy Patel <140176910+happy1511@users.noreply.github.com>
2024-06-23 12:30:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+ app.use(cors());
 
 const transport = nodeMailer.createTransport(
   {
